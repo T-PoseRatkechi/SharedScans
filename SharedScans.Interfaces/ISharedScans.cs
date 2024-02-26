54546d756d60e18b8caf27b67e4813c5f3c0fb18
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-26 07:36:56 -0600

    Add interfaces project.

---
+++ SharedScans.Interfaces/ISharedScans.cs
@@ -0,0 +1,25 @@
+﻿namespace SharedScans.Interfaces;
+
+public interface ISharedScans
+{
+    void AddScan<TFunction>(string id, string pattern);
+
+    void AddScan<TFunction>(string pattern);
+
+    HookContainer<TFunction> CreateHook<TFunction>(string id, Action<nint> success);
+
+    HookContainer<TFunction> CreateHook<TFunction>(string modName, TFunction hookFunc);
+}
+
+public class HookContainer<TFunction>
+{
+    public string Id { get; set; }
+
+    public string Pattern { get; set; }
+
+    public nint Address { get; set; }
+
+    public object? HookInstance { get; set; }
+
+    public TFunction Method { get; set; }
+}
