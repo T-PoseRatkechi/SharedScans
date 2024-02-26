9f97eef7fb9c6fd7be42d3f0c65789334f26698a
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-26 07:38:39 -0600

    Implement mod.

--- SharedScans.Reloaded/Mod.cs
+++ SharedScans.Reloaded/Mod.cs
@@ -1,44 +1,61 @@
 ﻿using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
+using SharedScans.Interfaces;
 using SharedScans.Reloaded.Configuration;
 using SharedScans.Reloaded.Template;
+using System.Diagnostics;
+using System.Drawing;
 
-namespace SharedScans.Reloaded
+namespace SharedScans.Reloaded;
+
+public class Mod : ModBase, IExports
 {
-    public class Mod : ModBase
+    private readonly IModLoader modLoader;
+    private readonly IReloadedHooks hooks;
+    private readonly ILogger log;
+    private readonly IMod owner;
+
+    private Config config;
+    private readonly IModConfig modConfig;
+
+    private readonly Scans.SharedScans scans;
+
+    public Mod(ModContext context)
     {
-        private readonly IModLoader modLoader;
-        private readonly IReloadedHooks? hooks;
-        private readonly ILogger log;
-        private readonly IMod owner;
+        this.modLoader = context.ModLoader;
+        this.hooks = context.Hooks!;
+        this.log = context.Logger;
+        this.owner = context.Owner;
+        this.config = context.Configuration;
+        this.modConfig = context.ModConfig;
 
-        private Config config;
-        private readonly IModConfig modConfig;
+        Log.Initialize("SharedScans", this.log, Color.White);
 
-        public Mod(ModContext context)
-        {
-            this.modLoader = context.ModLoader;
-            this.hooks = context.Hooks;
-            this.log = context.Logger;
-            this.owner = context.Owner;
-            this.config = context.Configuration;
-            this.modConfig = context.ModConfig;
-        }
+#if DEBUG
+        Debugger.Launch();
+#endif
 
-        #region Standard Overrides
-        public override void ConfigurationUpdated(Config configuration)
-        {
-            // Apply settings from configuration.
-            // ... your code here.
-            config = configuration;
-            log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
-        }
-        #endregion
-
-        #region For Exports, Serialization etc.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public Mod() { }
-#pragma warning restore CS8618
-        #endregion
+        this.modLoader.GetController<IStartupScanner>().TryGetTarget(out var scanner);
+        this.scans = new(this.hooks);
+        ScanHooks.Initialize(scanner!, this.hooks);
     }
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        config = configuration;
+        log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
+    }
+
+    public Type[] GetTypes() => new Type[] { typeof(ISharedScans) };
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
 }
---
+++ SharedScans.Reloaded/Scans/SharedScans.cs
@@ -0,0 +1,56 @@
+﻿using Reloaded.Hooks.Definitions;
+using SharedScans.Interfaces;
+
+namespace SharedScans.Reloaded.Scans;
+
+internal class SharedScans : ISharedScans
+{
+    private readonly IReloadedHooks hooks;
+    private readonly List<object> listeners = new();
+
+    public SharedScans(IReloadedHooks hooks)
+    {
+        this.hooks = hooks;
+    }
+
+    public void AddScan<TFunction>(string id, string pattern)
+    {
+        ScanHooks.Add(id, pattern, (hooks, result) =>
+        {
+            foreach (var listener in this.listeners)
+            {
+                if (listener is Listener<TFunction> list && list.Id == id)
+                {
+                    list.Hook.Pattern = pattern;
+                    list.Hook.Address = result;
+                    list.Hook.HookInstance = this.hooks.CreateHook(list.Hook.Method, result).Activate();
+
+                    Log.Information($"Hook created || {id} || For: {list.Name}");
+                }
+            }
+        });
+    }
+
+    public void AddScan<TFunction>(string pattern)
+        => this.AddScan<TFunction>(typeof(TFunction).Name, pattern);
+
+    public HookContainer<TFunction> CreateHook<TFunction>(string id, Action<nint> success)
+    {
+        throw new NotImplementedException();
+    }
+
+    public HookContainer<TFunction> CreateHook<TFunction>(string modName, TFunction hookFunc)
+    {
+        var id = typeof(TFunction).Name;
+        var hook = new HookContainer<TFunction>()
+        {
+            Id = id,
+        };
+
+        var listener = new Listener<TFunction>(modName, id, hook);
+        this.listeners.Add(listener);
+        return listener.Hook;
+    }
+
+    private record Listener<TFunction>(string Name, string Id, HookContainer<TFunction> Hook);
+}
